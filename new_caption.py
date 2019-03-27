c6d89ba312e914add20df5c355c1e279d9885870
Anant Joshi <anantjoshi1324@gmail.com>
2019-03-27 22:37:12 +0530

    Modify script to use diverse-beam-search
    Untested code, not final

--- new_caption.py
+++ new_caption.py
@@ -20,15 +20,15 @@ def __add_diversity(
         penalty_lambda,
         group_size):
     """ Adds a diversity penalty to a group of beams
-    beam_seqs : array containing beam sequences staggered across time
-    logprobs            : log probabilities for the beam sequences
-    time                : Current time unit (not adjusted for the current group
-    group_number        : the current group number
-    penalty_lambda      : diversity penalty
-    group_size          : num_beams/num_groups
+    beam_seqs       : array containing beam sequences staggered across time
+    logprobs        : log probabilities for the beam sequences
+    time            : Current time unit (not adjusted for the current group
+    group_number    : the current group number
+    penalty_lambda  : diversity penalty
+    group_size      : num_beams/num_groups
     """
     local_time = time - group_number # current time for the group
-    aug_logprobs = logprobs.clone()
+    aug_logprobs = logprobs.clone().to(device)
     for previous_choice in range(group_number):
         previous_decisions = beam_seqs[previous_choice][local_time]
         for beam in range(group_size):
@@ -77,7 +77,7 @@ def __beam_step(
                 }
             )
     candidates.sort(key=lambda x: x['prob'])
-    new_state = [x.clone() for x in state]
+    new_state = [x.clone().to(device) for x in state]
 
     if time > 1:
         beam_seq_prev = beam_seq[0:time,:].clone()
@@ -117,11 +117,10 @@ def diverse_beam_search(
         num_beams,
         num_groups,
         penalty_lambda,
-        state_size,
-        rnn_size,
         seq_length,
         end_token,
-        gen_logprobs):
+        gen_logprobs,
+        init_tokens=None):
     """ Performs Diverse Beam Search
     seq_length: maximum length of sequence
     rnn_state: states of the RNNs
@@ -129,7 +128,6 @@ def diverse_beam_search(
     num_beams: number of beams
     num_groups: number of groups
     penalty_lambda: value of diversity penalty
-    state_size: size of the RNN state
     end_token: end-token of the vocabulary
     gen_logprobs: function that returns
     the states and logprobs from the RNN output
@@ -142,9 +140,9 @@ def diverse_beam_search(
     done_beams = []
     logprobs_list=[]
     state=[]
-    for sub_beam_ix in range(beam_ratio):
-
     # Initialization
+
+    state = [[] for i in range(beam_ratio)]
 
     for group_num in range(num_groups):
         beam_seqs[group_num] = torch.zeros(
@@ -165,16 +163,23 @@ def diverse_beam_search(
         ).to(device)
 
         for sub_beam_ix in range(beam_ratio):
-            logprobs_list[group_num][sub_beam_ix] = init_logprobs.clone()
-            state[sub_beam_ix] = init_state.clone().to(device)
+            logprobs_list[group_num][sub_beam_ix] = (
+                init_logprobs.clone().to(device)
+            )
+            beam_seqs[group_num[sub_beam_ix]]= init_tokens.clone().to(device)
+            state[sub_beam_ix] = [st.clone().to(device) for st in init_state]
 
-        states[group_num] = [st.clone().to(device) for st in state]
+        states[group_num] = [
+            [item.clone().to(device) for item in st]
+            for st
+            in state
+        ]
     # End initialization
 
     for time in range(seq_length + num_groups):
         for group_ix in range(num_groups):
-            if time >=group_ix and time < seq_length + group_ix:
-                logprobss = logprobs_list[group_ix]
+            if time >= group_ix and time < seq_length + group_ix:
+                logprobs = logprobs_list[group_ix]
 
                 # Suppress <UNK> tokens in the decoding
                 logprobs[:,-1] -= 1000
@@ -244,15 +249,22 @@ def diverse_beam_search(
                     if is_first_end_token:
                         beam_logprobs_sum[group_ix][beam_ix] = -1000
 
-
-                inpt = beam_seqs[group_ix][time - group_ix]
+                inpt = beam_seqs[group_ix]
                 output = gen_logprobs(inpt, states[group_ix])
                 logprobs[group_ix] = out[-1].clone()
-                temp_state = [out[i] for i in range(state_size)]
+                temp_state = [
+                    out[i].clone().to(device)
+                    for i
+                    in range(len(output)-1)
+                ]
+                states[group_ix] = [st.clone().to(device) for st in temp_state]
+
     for i in range(num_groups):
-        done_beams_table[i].sort(key=lambda x:x['aug_logp'])
-        done_beams_table[i] = done_beams_table[i][0:beam_ratio]
-    return done_beams_table
+        done_beams[i].sort(key=lambda x:x['aug_logp'])
+        done_beams[i] = done_beams[i][0:beam_ratio]
+    return done_beams
+
+
 
 
 def caption_image_beam_search(
@@ -260,7 +272,8 @@ def caption_image_beam_search(
         decoder,
         image_path,
         word_map,
-        beam_size=3):
+        beam_size=8,
+        group_size=8):
     """Reads an image and captions it with beam search.
     param encoder: encoder model
     param decoder: decoder model
@@ -329,6 +342,7 @@ def caption_image_beam_search(
         enc_image_size
     ).to(device)  # (k, 1, enc_image_size, enc_image_size)
 
+
     # Lists to store completed sequences, their alphas and scores
     complete_seqs = list()
     complete_seqs_alpha = list()
@@ -339,120 +353,35 @@ def caption_image_beam_search(
     h, c = decoder.init_hidden_state(encoder_out)
     penalties = torch.zeros(seqs.size()[0], vocab_size).cuda()
 
+    def generate_log_probabilities(inputs, states):
+        h_ = states[0]
+        c_ = states[1]
+        embeddings = decoder.embedding(inputs).squeeze(1)
+        awe, alpha = decoder.attention(encoder_out, h_)
+        gate = decoder.sigmoid(decoder.f_beta(h_))
+        awe = gate * awe
+        h_, c_ = decoder.decode_step(
+            torch.cat([embeddings, awe], dim=1),
+            (h_, c_)
+        )
+        scores = F.log_softmax(decoder.fc(h_), dim=1)
+        return [h_, c_, scores]
+
+    done_beams = diverse_beam_search(
+        rnn_state=[h, c],
+        init_logprobs,
+        num_beams=k,
+        num_groups=g,
+        penalty_lambda=penalty,
+        seq_length=50,
+        end_token=word_map['<end>'],
+        gen_logprobs=generate_log_probabilities,
+        init_tokens=k_prev_words,
+    )
     # s is a number less than or equal to k,
     #because sequences are removed from this process once they hit <end>
-    while True:
-        embeddings = decoder.embedding(k_prev_words).squeeze(1)  #(s, embed_dim)
-        awe, alpha = decoder.attention(encoder_out, h)
-        # (s, encoder_dim), (s, num_pixels)
-
-        alpha = alpha.view(-1, enc_image_size, enc_image_size)
-        # (s, enc_image_size, enc_image_size)
-
-        gate = decoder.sigmoid(
-            decoder.f_beta(h)
-        )  # gating scalar, (s, encoder_dim)
-        awe = gate * awe
-
-        h, c = decoder.decode_step(
-            torch.cat([embeddings, awe], dim=1), (h, c)
-        )  # (s, decoder_dim)
-
-        scores = decoder.fc(h)  # (s, vocab_size)
-        scores = F.log_softmax(scores, dim=1)
-
-        print("Scores: {0}".format(scores.size()))
-        print("sequences: {0}".format(seqs.size()))
-
-
-        for b1 in range(seqs.size()[0]): # Iterate over each beam:
-            #B1 is the current beam we are evaluating
-            for b2 in range(b1): # To compare B1 with every previous beam B2
-                for token in seqs[b2]: # Check every token in b2
-                    if token in seqs[b1]: # if token exists in b1 AND b2
-                        # penalties[b1][:] += 1 # penalize that token
-                        penalties[b1][token] += 1
-
-        # Add diversity penalty
-        # scores = scores - args._lambda * penalties
-        scores = (
-            top_k_scores.expand_as(scores)
-            + scores
-            - (args._lambda * penalties))
-            / (0.7*seqs.size()[1]
-        )# (s, vocab_size)
-
-        # For the first step, all k points will have
-        #the same scores (since same k previous words, h, c)
-
-        print(scores.size())
-        print(scores - args._lambda * penalties )
-        if step == 1:
-        top_k_scores, top_k_words = scores[0].topk(k, 0, True, True)  # (s)
-        else:
-        # Unroll and find top scores, and their unrolled indices
-            top_k_scores, top_k_words = (
-                scores.view(-1).topk(k, 0, True, True)
-            )  # (s)
-
-        # Convert unrolled indices to actual indices of scores
-        prev_word_inds = top_k_words / vocab_size  # (s)
-        next_word_inds = top_k_words % vocab_size  # (s)
-
-        # Add new words to sequences, alphas
-        seqs = torch.cat(
-            [seqs[prev_word_inds],
-             next_word_inds.unsqueeze(1)],
-            dim=1
-        )  # (s, step+1)
-        seqs_alpha = torch.cat(
-            [seqs_alpha[prev_word_inds],
-             alpha[prev_word_inds].unsqueeze(1)],
-            dim=1
-        )  # (s, step+1, enc_image_size, enc_image_size)
-
-        # Which sequences are incomplete (didn't reach <end>)?
-        incomplete_inds = [
-            ind
-            for ind, next_word
-            in enumerate(next_word_inds)
-            if next_word != word_map['<end>']
-        ]
-
-        complete_inds = (
-            list(
-                set(range(len(next_word_inds)))
-                - set(incomplete_inds)
-            )
-        )
-
-        # Set aside complete sequences
-        if len(complete_inds) > 0:
-            complete_seqs.extend(seqs[complete_inds].tolist())
-            complete_seqs_alpha.extend(seqs_alpha[complete_inds].tolist())
-            complete_seqs_scores.extend(top_k_scores[complete_inds])
-            k -= len(complete_inds)  # reduce beam length accordingly
-
-        # Proceed with incomplete sequences
-        if k == 0:
-            break
-        seqs = seqs[incomplete_inds]
-        seqs_alpha = seqs_alpha[incomplete_inds]
-        h = h[prev_word_inds[incomplete_inds]]
-        c = c[prev_word_inds[incomplete_inds]]
-        encoder_out = encoder_out[prev_word_inds[incomplete_inds]]
-        top_k_scores = top_k_scores[incomplete_inds].unsqueeze(1)
-        k_prev_words = next_word_inds[incomplete_inds].unsqueeze(1)
-
-        # Break if things have been going on too long
-    if step > 50:
-        break
-    step += 1
-    i = complete_seqs_scores.index(max(complete_seqs_scores))
-    seq = complete_seqs
-    alphas = complete_seqs_alpha[i]
-
-    return seq, alphas
+    seq = [beam['seq'] for beam in done_beams]
+    return seq
 
 
 def visualize_att(image_path, seqs, alphas, rev_word_map, smooth=True):
@@ -495,17 +424,44 @@ def visualize_att(image_path, seqs, alphas, rev_word_map, smooth=True):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Show, Attend, and Tell - Tutorial - Generate Caption')
+    parser = argparse.ArgumentParser(
+        description='Show, Attend, and Tell - Tutorial - Generate Caption'
+    )
+    parser.add_argument('--img', '-i', help='path to image')
+    parser.add_argument('--model', '-m', help='path to model')
+    parser.add_argument('--word_map', '-wm', help='path to word map JSON')
+    parser.add_argument(
+        '--beam_size',
+        '-b',
+        dest="beam_size",
+        default=8,
+        type=int,
+        help='beam size for beam search'
+    )
 
-    parser.add_argument(':img', '-i', help='path to image')
-    parser.add_argument(':model', '-m', help='path to model')
-    parser.add_argument(':word_map', '-wm', help='path to word map JSON')
-    parser.add_argument(':beam_size', '-b', default=5, type=int, help='beam size for beam search')
-    parser.add_argument(':dont_smooth', dest='smooth', action='store_false', help='do not smooth alpha overlay')
-    parser.add_argument(':lambda', default=0.5, type=float, dest="_lambda", help="Diversity parameter for diverse beam search")
+    parser.add_argument(
+        '--group_size',
+        '-b',
+        dest="group_size",
+        default=8,
+        type=int,
+        help='beam size for beam search'
+    )
+    parser.add_argument(
+        '--dont_smooth',
+        dest='smooth',
+        action='store_false',
+        help='do not smooth alpha overlay'
+    )
+    parser.add_argument(
+        ':lambda',
+        default=0.5,
+        type=float,
+        dest="_lambda",
+        help="Diversity parameter for diverse beam search"
+    )
 
     args = parser.parse_args()
-
     # Load model
     checkpoint = torch.load(args.model)
     decoder = checkpoint['decoder']
@@ -521,12 +477,17 @@ if __name__ == '__main__':
     rev_word_map = {v: k for k, v in word_map.items()}  # ix2word
 
     # Encode, decode with attention and beam search
-    seqs, alphas = caption_image_beam_search(encoder, decoder, args.img, word_map, args.beam_size)
-    alphas = torch.FloatTensor(alphas)
-
+    seqs = caption_image_beam_search(
+        encoder,
+        decoder,
+        image_path,
+        word_map,
+        beam_size=args['beam_size'],
+        group_size=args['group_size']
+    )
     for seq in seqs:
     words = [rev_word_map[ind] for ind in seq]
     print(words)
 
     # # Visualize caption and attention of best sequence
-                                            # visualize_att(args.img, seqs, alphas, rev_word_map, args.smooth)
+    # visualize_att(args.img, seqs, alphas, rev_word_map, args.smooth)
